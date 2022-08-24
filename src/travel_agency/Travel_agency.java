06d24e0789a5deb5b0e1bcad50011916e8d8b8c7
Samarthmeghani <>
2022-08-25 00:49:08 +0530

    Java Travel Agency

---
+++ src/travel_agency/Travel_agency.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travel_agency;
+
+/**
+ *
+ * @author Dell
+ */
+public class Travel_agency {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new main_menu().setVisible(true);
+            }
+        });
+    }
+    
+}
